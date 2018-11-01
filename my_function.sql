a8ab79522f82465f78029c12f30bc507a00ce8d8
xlsnoopy <40174160+xlsnoopy@users.noreply.github.com>
2018-11-01 19:13:40 -0400

    Add files via upload

---
+++ my_function.sql
@@ -0,0 +1,8 @@
+create function freq (x char(8), y char(8))
+	returns varchar(28)
+	begin
+		declare s char(28);
+		SELECT rel, COUNT(rel) as count from wn18rr_train where ein = @x or eout = @x GROUP BY rel ORDER BY count DESC LIMIT 1 A;
+		SELECT rel, COUNT(rel) as count from wn18rr_train where ein = @y or eout = @y GROUP BY rel ORDER BY count DESC LIMIT 1 B;
+		set s = if(A.count > B.count, A.rel, B.rel);
+		return s;
